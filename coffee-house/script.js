a59de5d74cf7989017c8a894e0c994e6c23db02f
Ana Gagnidze <anagagnidze32@gmail.com>
2024-01-14 23:17:33 +0400

    added week3 functionality

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -1,14 +1,27 @@
 
-    const track = document.querySelector('.carouseltrack');
-    const slides = Array.from(track.children);
-    const nextbtn = document.querySelector('.carousel_button--right');
-    const prevbtn = document.querySelector('.carousel_button--left');
-    const dotsnav = document.querySelector('.carouselnav');
-    const dots = Array.from(dotsnav.children);
+const track = document.querySelector('.carouseltrack');
+const slides = Array.from(track.children);
+const nextbtn = document.querySelector('.carousel_button--right');
+const prevbtn = document.querySelector('.carousel_button--left');
+const dotsnav = document.querySelector('.carouselnav');
+const dots = Array.from(dotsnav.children);
 
-    
+const slideWidth = slides[0].getBoundingClientRect().width;  
 
- const slideWidth = slides[0].getBoundingClientRect().width;  
+let autoScrollInterval;
+const moveToNextSlide = () => {
+    const current = track.querySelector('.current-slide');
+    const nextSlide = current.nextElementSibling || slides[0];
+    const currentDot = dotsnav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling || dots[0];
+
+    moveToSlide(track, current, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevbtn, nextbtn, nextIndex);
+};
+autoScrollInterval = setInterval(moveToNextSlide, 5000);
+
+
 
 
 const setSlidePosition = (slide, index) => {
@@ -30,7 +43,7 @@ const hideShowArrows = (slides, prevbtn, nextbtn, targetIndex) => {
     if(targetIndex === 0){
         prevbtn.classList.add('is-hidden');
         nextbtn.classList.remove('is-hidden');
-    }else if(targetIndex === slides.lenght - 1){
+    }else if(targetIndex === slides.length - 1){
         prevbtn.classList.remove('is-hidden');
         nextbtn.classList.add('is-hidden');
     }else{
@@ -39,32 +52,69 @@ const hideShowArrows = (slides, prevbtn, nextbtn, targetIndex) => {
     }
 }
 
+let touchStartX = 0;
+let touchEndX = 0;
+
+
+track.addEventListener('touchstart', function (e) {
+    touchStartX = e.touches[0].clientX;
+});
+
+track.addEventListener('touchmove', function (e) {
+    touchEndX = e.touches[0].clientX;
+});
+
+track.addEventListener('touchend', function () {
+    const swipeThreshold = 50; 
+
+    if (touchStartX - touchEndX > swipeThreshold) {
+        const current = track.querySelector('.current-slide');
+        const nextSlide = current.nextElementSibling || slides[0];
+        const currentDot = dotsnav.querySelector('.current-slide');
+        const nextDot = currentDot.nextElementSibling || dots[0];
+
+        moveToSlide(track, current, nextSlide);
+        updateDots(currentDot, nextDot);
+        hideShowArrows(slides, prevbtn, nextbtn, nextIndex);
+    } else if (touchEndX - touchStartX > swipeThreshold) {
+        const current = track.querySelector('.current-slide');
+        const prevSlide = current.previousElementSibling || slides[slides.length - 1];
+        const currentDot = dotsnav.querySelector('.current-slide');
+        const prevDot = currentDot.previousElementSibling || dots[dots.length - 1];
+        const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+        moveToSlide(track, current, prevSlide);
+        updateDots(currentDot, prevDot);
+        hideShowArrows(slides, prevbtn, nextbtn, prevIndex);
+    }
+});
 
 nextbtn.addEventListener('click', e => {
     const current = track.querySelector('.current-slide');
-    const nextSlide = current.nextElementSibling;
+    const nextSlide = current.nextElementSibling || slides[0];
     const currentDot = dotsnav.querySelector('.current-slide');
-    const nextDot = currentDot.nextElementSibling;
-    const nextIndex = slides.findIndex(slide => slide === nextSlide);
-
+    const nextDot = currentDot.nextElementSibling || dots[0];
+    
     moveToSlide(track, current, nextSlide);
     updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevbtn, nextbtn, nextIndex);
    
     
 })
 
 prevbtn.addEventListener('click', e => {
     const current = track.querySelector('.current-slide');
-    const prevSlide = current.previousElementSibling;
+    const prevSlide = current.previousElementSibling || slides[slides.length - 1];
     const currentDot = dotsnav.querySelector('.current-slide');
-    const prevDot = currentDot.previousElementSibling;
+    const prevDot = currentDot.previousElementSibling || dots[dots.length - 1];
     const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
     
     moveToSlide(track, current, prevSlide);
     updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevbtn, nextbtn, prevIndex);
    
 })
-
 
 dotsnav.addEventListener('click', e =>{
     const targetDot = e.target.closest('button');
@@ -79,3 +129,6 @@ dotsnav.addEventListener('click', e =>{
     moveToSlide(track, current, targetSlide);
     updateDots(currentDot, targetDot);  
 })
+
+
+const navigation = document.querySelector('.navigation');
